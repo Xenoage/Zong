b7579e7ce1d9006abde511b1376e16d8fd3f5f47
Andreas Wenger <andi@xenoage.com>
2013-09-23 07:58:01 +0200

    Removed lib updater for Linux and updated cross-platform lib updater based on Ant

--- update-lib.sh
+++
@@ -1,6 +0,0 @@
-echo "Downloading Zong! libraries..."
-rm lib/*.jar lib/*.txt
-for line in `cat lib/lib.list`; do
- wget --directory-prefix=lib $line
-done
-echo "Downloading Zong! libraries finished."
