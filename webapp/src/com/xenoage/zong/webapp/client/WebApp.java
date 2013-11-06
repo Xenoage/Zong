0f2f62cf5686189ed2f62e96444cb3fff15bf8a1
Andreas Wenger <andi@xenoage.com>
2013-11-06 08:33:37 +0100

    ZONG-29 continued: Debug successfull, compile fails (Java SE dependencies in utils project)

--- webapp/src/com/xenoage/zong/webapp/client/WebApp.java
+++ webapp/src/com/xenoage/zong/webapp/client/WebApp.java
@@ -6,8 +6,6 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.xenoage.zong.core.Score;
-import com.xenoage.zong.core.music.clef.Clef;
-import com.xenoage.zong.core.music.clef.ClefType;
 import com.xenoage.zong.core.music.util.Interval;
 import com.xenoage.zong.core.position.MP;
 import com.xenoage.zong.util.demo.ScoreRevolutionary;
@@ -30,14 +28,16 @@ public class WebApp
 		//String t = new Point2f(5, 10).toString();
 		//Range r = Range.range(5);
 		//Label hello = new Label("Result: " + t); //"Aha "+r.getCount() + " " + Defaults.defaultFont.getSize() + " " + t);
-		RootPanel.get("container").add(new Label(findAClef(score)));
+		RootPanel container = RootPanel.get("container");
+		container.add(new Label(findAClef(score, MP.atBeat(0, 1, 0, _0))));
+		container.add(new Label(findAClef(score, MP.atBeat(1, 1, 0, _0))));
+		MP mp = MP.atBeat(0, 1, 0, _0);
+		container.add(new Label("Voice at " + mp + ": " + score.getVoice(mp)));
 	}
 	
-	private String findAClef(Score score) {
-		return "Last clef at or before measure 1 in staff 0: " +
-			score.getClef(MP.atBeat(0, 1, 0, _0), Interval.BeforeOrAt).getType().toString() + "<br/>" +
-			"Last clef at or before measure 1 in staff 1: " +
-			score.getClef(MP.atBeat(1, 1, 0, _0), Interval.BeforeOrAt).getType().toString();
+	private String findAClef(Score score, MP mp) {
+		return "Last clef at or before " + mp + ": " +
+			score.getClef(mp, Interval.BeforeOrAt).getType().toString();
 	}
 	
 }
