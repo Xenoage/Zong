3eb5b58e76ffb361ed0617d78602d9a0dda7513b
Andreas Wenger <andi@xenoage.com>
2014-11-13 09:00:27 +0100

    MusicXML test 45e

---
+++ test/test/musicxmltestsuite/tests/musicxmlin/Test45e.java
@@ -0,0 +1,25 @@
+package musicxmltestsuite.tests.musicxmlin;
+
+import static musicxmltestsuite.tests.utils.ScoreTest.assertEqualsEndBarlines;
+import static musicxmltestsuite.tests.utils.ScoreTest.assertEqualsStartBarlines;
+import static musicxmltestsuite.tests.utils.ScoreTest.assertEqualsVoltas;
+import musicxmltestsuite.tests.base.Base45e;
+
+import org.junit.Test;
+
+import com.xenoage.zong.core.Score;
+
+
+public class Test45e
+	implements Base45e, MusicXmlInTest {
+	
+	@Test public void test() {
+		Score score = getScore();
+		assertEqualsStartBarlines(expectedStartBarlines, score);
+		assertEqualsEndBarlines(expectedEndBarlines, score);
+		assertEqualsVoltas(expectedVoltas, score);
+	}
+
+	
+	
+}
