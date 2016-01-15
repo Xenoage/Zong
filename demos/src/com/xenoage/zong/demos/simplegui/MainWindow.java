8dfcf214e70a694673f29f11186c7dd93fc88c1a
Andreas Wenger <andi@xenoage.com>
2016-01-15 23:04:53 +0100

    Updated to JavaFX 8 dialogs

--- demos/src/com/xenoage/zong/demos/simplegui/MainWindow.java
+++ demos/src/com/xenoage/zong/demos/simplegui/MainWindow.java
@@ -11,14 +11,13 @@ import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-
-import org.controlsfx.dialog.Dialog;
-import org.controlsfx.dialog.Dialogs;
 
 
 /**
@@ -112,11 +111,15 @@ public class MainWindow {
 	}
 	
 	public void showMessageDialog(String message) {
-		dialog().message(message).showInformation();
+		Alert dialog = dialog();
+		dialog.setContentText(message);
+		dialog.showAndWait();
 	}
 	
-	private Dialogs dialog() {
-		return Dialogs.create().title(SimpleGuiDemo.appName).styleClass(Dialog.STYLE_CLASS_NATIVE);
+	private Alert dialog() {
+		Alert ret = new Alert(AlertType.INFORMATION);
+		ret.setTitle(SimpleGuiDemo.appName);
+		return ret;
 	}
 
 }
--- desktop/src/com/xenoage/zong/desktop/utils/FilenameDialogFilter.java
+++ desktop/src/com/xenoage/zong/desktop/utils/FilenameDialogFilter.java
@@ -10,6 +10,8 @@ import com.xenoage.utils.filter.Filter;
 import com.xenoage.utils.lang.Lang;
 import com.xenoage.zong.Voc;
 
+import javafx.scene.control.ChoiceDialog;
+
 /**
  * Filter dialog which allows selecting a single filename.
  * When there is only one file given, it is chosen without
@@ -28,8 +30,9 @@ public class FilenameDialogFilter
 			ret.addAll(values);
 		}
 		else {
-			Optional<String> selectedFile = app().dialog().message(
-				Lang.getLabel(Voc.SelectDocument)).showChoices(values);
+			ChoiceDialog<String> dialog = new ChoiceDialog<>(values.get(0), values); //TODO: use Zong! dialog factory
+			dialog.setContentText(Lang.getLabel(Voc.SelectDocument));
+			Optional<String> selectedFile = dialog.showAndWait();
 			//open file
 			if (selectedFile.isPresent()) {
 				ret.add(selectedFile.get());
