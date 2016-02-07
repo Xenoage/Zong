b797497819cd81d9c3a8d501c8cb65284788520c
Andreas Wenger <andi@xenoage.com>
2016-02-07 10:30:18 +0100

    Restored ContinuedWedge

---
+++ layout/src/com/xenoage/zong/musiclayout/continued/ContinuedWedge.java
@@ -0,0 +1,20 @@
+package com.xenoage.zong.musiclayout.continued;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import com.xenoage.utils.annotations.Const;
+import com.xenoage.zong.core.music.direction.Wedge;
+
+/**
+ * Continued {@link Wedge}.
+ * 
+ * @author Andreas Wenger
+ */
+@Const @AllArgsConstructor
+public final class ContinuedWedge
+	implements ContinuedElement {
+
+	@Getter public final Wedge element;
+
+}
