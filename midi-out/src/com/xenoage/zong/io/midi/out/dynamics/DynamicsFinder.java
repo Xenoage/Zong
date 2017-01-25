95c14c48a016946297ce4987fd5c8ab07eb01b59
Andreas Wenger <andi@xenoage.com>
2017-01-25 17:31:36 +0100

    midi-out: DynamicsFinder improved

--- midi-out/src/com/xenoage/zong/io/midi/out/dynamics/DynamicsFinder.java
+++ midi-out/src/com/xenoage/zong/io/midi/out/dynamics/DynamicsFinder.java
@@ -6,6 +6,7 @@ import com.xenoage.zong.core.music.MusicElementType;
 import com.xenoage.zong.core.music.direction.Dynamic;
 import com.xenoage.zong.core.music.direction.DynamicValue;
 import com.xenoage.zong.core.music.direction.Wedge;
+import com.xenoage.zong.core.music.direction.WedgeEnd;
 import com.xenoage.zong.core.position.Time;
 import com.xenoage.zong.io.midi.out.dynamics.type.DynamicsType;
 import com.xenoage.zong.io.midi.out.dynamics.type.FixedDynamics;
@@ -86,11 +87,22 @@ public class DynamicsFinder {
 				if ((iMeasure == rep.start.measure && beat.compareTo(rep.start.beat) < 0) ||
 						(iMeasure == rep.end.measure && beat.compareTo(rep.end.beat) >= 0))
 					continue;
-				//wedge ending at this beat?
-				//GOON
-				//new dynamic starting? then close currently open period and open new one
+				DynamicsType newDynamic = null;
 				val newTime = time(iMeasure, beat);
-				DynamicsType newDynamic = getStaffDynamicsAt(staff, newTime, currentDynamic.getEndValue());
+				//find dynamics change
+				if (currentDynamic instanceof GradientDynamics) {
+					//wedge ending at this beat?
+					val closedWedge = getWedgeEndAt(staff, newTime, (GradientDynamics) currentDynamic);
+					if (closedWedge != null) {
+						currentDynamic = closedWedge;
+						newDynamic = new FixedDynamics(closedWedge.end); //continue with the end dynamic of the wedge
+					}
+				}
+				else if (newDynamic == null) {
+					//new dynamic starting? then close currently open period and open new one
+					newDynamic = getStaffDynamicsAt(staff, newTime, currentDynamic.getEndValue());
+				}
+				//when change was found, apply it
 				if (newDynamic != null) {
 					if (false == currentStartTime.equals(newTime) && false == currentDynamic.equals(newDynamic)) {
 						val period = new DynamicsPeriod(currentStartTime, newTime, currentDynamic);
@@ -110,7 +122,7 @@ public class DynamicsFinder {
 	}
 
 	/**
-	 * Returns the {@link DynamicsType} starting at the given position within a staff (not a voice),
+	 * Returns the {@link DynamicsType} starting at the given time within the given staff (not a voice),
 	 * or null, if nothing starts here.
 	 */
 	@MaybeNull private DynamicsType getStaffDynamicsAt(int staff, Time time, DynamicValue currentDynamicValue) {
@@ -140,6 +152,36 @@ public class DynamicsFinder {
 		else if (foundDynamic != null) {
 			//fixed value
 			return new FixedDynamics(foundDynamic.getValue());
+		}
+
+		//nothing found
+		return null;
+	}
+
+	/**
+	 * Checks, if there is a {@link WedgeEnd} at the given time within the given staff (not a voice).
+	 * If yes, the given open wedge is closed and returned.
+	 */
+	@MaybeNull private GradientDynamics getWedgeEndAt(int staff, Time time, GradientDynamics openWedge) {
+		val measure = score.getMeasure(atMeasure(staff, time.measure));
+
+		//are there dynamics at all?
+		if (measure.getDirections() == null)
+			return null;
+		val dirs = measure.getDirections().getAll(time.beat);
+
+		//when there is a WedgeEnd (possible with a Dynamic as the end volume), end the wedge
+		Dynamic foundDynamic = null;
+		WedgeEnd foundWedgeEnd = null;
+		for (val dir : dirs) {
+			if (MusicElementType.Dynamic.is(dir))
+				foundDynamic = (Dynamic) dir;
+			else if (MusicElementType.WedgeEnd.is(dir))
+				foundWedgeEnd = (WedgeEnd) dir;
+		}
+		if (foundWedgeEnd != null) {
+			val endDynamicValue = (foundDynamic != null ? foundDynamic.getValue() : openWedge.end);
+			return new GradientDynamics(openWedge.start, endDynamicValue);
 		}
 
 		//nothing found
