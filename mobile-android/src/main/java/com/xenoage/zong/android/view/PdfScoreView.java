ef7906dd211f997e8f92f5753f4e8430ec2a39b9
Andreas Wenger <andi@xenoage.com>
2017-05-19 16:05:59 +0200

    ZONG-121: Using PdfRenderer (buggy, replace it)

--- mobile-android/src/main/java/com/xenoage/zong/android/view/PdfScoreView.java
+++ mobile-android/src/main/java/com/xenoage/zong/android/view/PdfScoreView.java
@@ -1,10 +1,18 @@
 package com.xenoage.zong.android.view;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.pdf.PdfRenderer;
+import android.os.ParcelFileDescriptor;
 
 import com.xenoage.utils.math.geom.Size2i;
+import com.xenoage.zong.android.R;
 
+import java.io.IOException;
+
+import static com.xenoage.utils.android.AndroidPlatformUtils.androidPlatformUtils;
 import static com.xenoage.utils.kernel.Range.range;
 
 
@@ -16,7 +24,20 @@ import static com.xenoage.utils.kernel.Range.range;
 public class PdfScoreView
 		extends ScoreView {
 
-	public PdfScoreView(String filepath, Size2i screenSizePx, float zoom) {
+	PdfRenderer renderer;
+	int pagesCount;
+
+	public PdfScoreView(String filepath, Size2i screenSizePx, float zoom)
+			throws IOException {
+		/* AssetFileDescriptor afd = androidPlatformUtils().getContext().getAssets().openFd("files/" + filepath); //TIDY
+		ParcelFileDescriptor pfd = afd.getParcelFileDescriptor();
+		renderer = new PdfRenderer(pfd); */
+		//GOON: use assets instead of res again, but change PDF library. the current one is buggy! and set back to min api level 15
+		//https://github.com/barteksc/PdfiumAndroid
+		Resources res = androidPlatformUtils().getContext().getResources();
+		ParcelFileDescriptor pfd = res.openRawResourceFd(R.raw.pdfsample).getParcelFileDescriptor();
+		renderer = new PdfRenderer(pfd);
+		pagesCount = renderer.getPageCount();
 		updateScreen(screenSizePx, zoom);
 	}
 
@@ -24,14 +45,14 @@ public class PdfScoreView
 	}
 
 	@Override public int getPagesCount() {
-		return 1;
+		return pagesCount;
 	}
 
 	@Override public PageView getPage(int pageIndex) {
-		Bitmap bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-		for (int x : range(200))
-			for (int y : range(200))
-				bitmap.setPixel(x, y, Color.RED);
+		Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
+		PdfRenderer.Page page = renderer.openPage(pageIndex);
+		page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+		page.close();
 		return new PageView(bitmap);
 	}
 
